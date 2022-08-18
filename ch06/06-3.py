196f2bb15c93155949f6a18687b57486b5f95ff8
sangjun-park-201520131 <89827411+bullmouse@users.noreply.github.com>
2022-08-18 14:09:34 +0900

    [FIX] LeetCode 버전으로 수정

--- ch06/06-3.py
+++ ch06/06-3.py
@@ -1,19 +1,16 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        alpha_logs = []
+        num_logs = []
 
+        # 문자로그와 숫자로그를 분리하기
+        for log in logs:
+            if log.split(' ')[1].isalpha():
+                alpha_logs.append(log)
+            else:
+                num_logs.append(log)
 
-def reorder_log_files(logs):
-    alpha_logs = []
-    num_logs = []
+        # 문자 로그 정렬 (숫자 로그는 정렬할 필요 없음)
+        alpha_logs.sort(key=lambda x: (x.split(' ')[1:], x.split(' ')[0]))
 
-    # 문자로그와 숫자로그를 분리하기
-    for log in logs:
-        if log.split(' ')[1].isalpha():
-            alpha_logs.append(log)
-        else:
-            num_logs.append(log)
-
-    # 문자 로그 정렬 (숫자 로그는 정렬할 필요 없음)
-    alpha_logs.sort(key=lambda x: (x.split(' ')[1:], x.split(' ')[0]))
-
-    return alpha_logs + num_logs
-
-reorder_log_files(["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"])
+        return alpha_logs + num_logs
