54074da398bc075675e380d79018f645d0973e03
sangjun-park-201520131 <89827411+bullmouse@users.noreply.github.com>
2022-08-20 15:43:10 +0900

    [FEAT] 챕터 6 문제 5 구현 완료

---
+++ ch06/06-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        str_sets = set()
+        for val in strs:
+            str_sets.add(''.join(sorted(list(val))))
+
+        strs_list = list(str_sets)
+        answer = [[] for i in range(len(strs_list))]
+
+        for val in strs:
+            answer[strs_list.index(''.join(sorted(list(val))))].append(val)
+
+        return answer
+
