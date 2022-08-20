cc6e8e446973e90b5b7744d4efe93ffecd8530c0
sangjun-park-201520131 <89827411+bullmouse@users.noreply.github.com>
2022-08-20 14:59:50 +0900

    [FEAT] 챕터 6 문제 4 구현 완료

---
+++ ch06/06-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        convert_paragraph = re.sub(r"[^a-z]+", ' ', paragraph.lower()).split()
+        print(convert_paragraph)
+
+        common_word_dict = dict()
+        filter_banned_array = [i for i in convert_paragraph
+                               if i not in banned]
+        for element in filter_banned_array:
+            if element in common_word_dict:
+                common_word_dict[element] += 1
+            else:
+                common_word_dict[element] = 1
+
+        return sorted(common_word_dict.items(), key=lambda x: x[1]).pop()[0]
