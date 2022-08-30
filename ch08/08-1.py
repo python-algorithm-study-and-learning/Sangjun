21478d21a8c42010633e53bc67f9cf881693ce62
bullmouse <sjpark960520@gmail.com>
2022-08-30 13:24:17 +0900

    챕터8 문제1 풀이 완료

---
+++ ch08/08-1.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        val_arr = []
+        current_pointer = head
+        
+        while current_pointer.next:
+            val_arr.append(current_pointer.val)
+            current_pointer = current_pointer.next
+            
+        val_arr.append(current_pointer.val)
+        
+        return val_arr == val_arr[::-1]
