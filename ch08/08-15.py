91f0f24746070c357e0c93f42400a120f99c3bbb
bullmouse <sjpark960520@gmail.com>
2022-08-31 08:47:43 +0900

    챕터8 문제15 구현 완료

---
+++ ch08/08-15.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        answer_cur = None
+        while head:
+            answer_cur, answer_cur.next, head = head, answer_cur, head.next
+        return answer_cur
