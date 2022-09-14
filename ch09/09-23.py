d20cf9541f234fc67265931fe0d22dca943bcf24
bullmouse <sjpark960520@gmail.com>
2022-09-14 09:42:22 +0900

    [FEAT] 23번 문제 풀이

---
+++ ch09/09-23.py
@@ -0,0 +1,21 @@
+class MyStack:
+    def __init__(self):
+        self.queue = collections.deque();
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft());
+        return self.queue.popleft();
+    
+    def top(self) -> int:
+        return self.queue[-1]
+        
+    def empty(self) -> bool:
+        if len(list(self.queue)) != 0: 
+            return False
+        else: 
+            return True
+        
