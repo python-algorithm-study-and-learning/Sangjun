c4b5a2271e1f92061626638f88993410cc4369d0
bullmouse <sjpark960520@gmail.com>
2022-09-20 22:38:41 +0900

    [FEAT] 28번 풀이 완료

---
+++ ch11/11-28.py
@@ -0,0 +1,22 @@
+class MyHashMap:
+    def __init__(self):
+        self.hashMap = collections.defaultdict(int)
+
+    def put(self, key: int, value: int) -> None:
+        self.hashMap[key] = value
+        
+    def get(self, key: int) -> int:
+        if key in self.hashMap:
+            return self.hashMap[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        if self.hashMap[key] != None:
+            del self.hashMap[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
