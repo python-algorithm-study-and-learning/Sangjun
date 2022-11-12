d19a84aa778ef1800209016cb43023bbf142e3a5
bullmouse <sjpark960520@gmail.com>
2022-11-12 16:14:00 +0900

    50번까지 문제 풀기

---
+++ ch14/my_solution_50.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+        
+        cur_idx = len(nums) // 2
+        cur_node = TreeNode(nums[cur_idx])
+        cur_left_idx = cur_idx // 2
+        cur_right_idx = (len(nums) + cur_idx) // 2
+        
+        if cur_node != cur_left_idx: 
+            cur_node.left = self.sortedArrayToBST(nums[:cur_idx])
+        if cur_node != cur_right_idx: 
+            cur_node.right = self.sortedArrayToBST(nums[cur_idx + 1:])
+            
+        return cur_node
